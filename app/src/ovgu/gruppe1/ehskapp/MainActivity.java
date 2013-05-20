384277ea487dd869bcf5df75f30f08006fa3823b
Darius Brückers <darius@brueckers>
2013-05-20 15:52:39 +0200

    ActionBarSherlock integration
    Wird auf Geräten <Android 4.0 für Actionbar gebruacht.
    Actionbar als Project from existing Source in Eclipse einfügen.
    Anleitungen:
    * http://actionbarsherlock.com/
    * https://www.youtube.com/watch?v=4GJ6yY1lNNY

--- app/src/ovgu/gruppe1/ehskapp/MainActivity.java
+++ app/src/ovgu/gruppe1/ehskapp/MainActivity.java
@@ -1,22 +1,13 @@
 package ovgu.gruppe1.ehskapp;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
