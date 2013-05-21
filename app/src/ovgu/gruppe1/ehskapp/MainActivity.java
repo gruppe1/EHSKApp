e19160175edccd41a0edfe290acaaaaa3e193039
Darius Brückers <darius@brueckers>
2013-05-21 10:17:06 +0200

    dispatched ABS again
    ABS wird vorerst nicht benötigt. Kann bei Bedarf einfach wieder
    angeschaltet werden.

--- app/src/ovgu/gruppe1/ehskapp/MainActivity.java
+++ app/src/ovgu/gruppe1/ehskapp/MainActivity.java
@@ -1,10 +1,9 @@
 package ovgu.gruppe1.ehskapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import com.actionbarsherlock.app.SherlockActivity;
-
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
