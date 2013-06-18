81cc707e1ff5a6341a47548cdc14f86078511c72
Darius Brückers <darius@brueckers>
2013-06-18 15:35:09 +0200

    added Preferences and MainActivity
    preferences:
    - code
    - time1 - time4

--- app/gen/ovgu/gruppe1/ehskapp/R.java
+++ app/gen/ovgu/gruppe1/ehskapp/R.java
@@ -8,6 +8,16 @@
 package ovgu.gruppe1.ehskapp;
 
 public final class R {
+    public static final class array {
+        public static final int time1=0x7f050000;
+        public static final int time1Value=0x7f050001;
+        public static final int time2=0x7f050002;
+        public static final int time2Value=0x7f050003;
+        public static final int time3=0x7f050004;
+        public static final int time3Value=0x7f050005;
+        public static final int time4=0x7f050006;
+        public static final int time4Value=0x7f050007;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,25 +27,36 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f0a0006;
+        public static final int btn_admin=0x7f0a0002;
+        public static final int btn_popup=0x7f0a0003;
+        public static final int btn_preferences=0x7f0a0001;
+        public static final int btn_x=0x7f0a0004;
+        public static final int linearLayout1=0x7f0a0000;
+        public static final int settings_text_view=0x7f0a0005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int show_settings_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_main=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f070001;
+        public static final int admin=0x7f070004;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
+        public static final int popup=0x7f070005;
+        public static final int preferences=0x7f070003;
     }
     public static final class style {
         /** 
@@ -59,10 +80,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
