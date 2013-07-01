74df4c161582be41240be2ecea59bcb894ddc691
Thomas <thomas@Thomas-Laptop.(none)>
2013-07-01 10:53:57 +0200

    added Question-Activity

---
+++ app/MainActivity/src/ovgu/gruppe1/ehskapp/MainActivity.java
@@ -0,0 +1,35 @@
+package ovgu.gruppe1.ehskapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button btn_preferences = (Button) findViewById(R.id.btn_preferences);
+		btn_preferences.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, QuickPrefsActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
