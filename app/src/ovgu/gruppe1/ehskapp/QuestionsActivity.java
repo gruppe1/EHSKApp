553b52b54814de21e679042784e9f9d85ab244c8
christiansh <evibu@vollbio.de>
2013-07-03 21:36:29 +0200

    layouts + questionsactivity + usercodeactivity

--- app/src/ovgu/gruppe1/ehskapp/QuestionsActivity.java
+++ app/src/ovgu/gruppe1/ehskapp/QuestionsActivity.java
@@ -1,73 +1,71 @@
 package ovgu.gruppe1.ehskapp;
 
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Locale;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnKeyListener;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.view.View.OnClickListener;
 
+public class QuestionsActivity extends Activity {
 
-public class QuestionsActivity extends Activity implements OnKeyListener {
-	
-	EditText numberOfContactsView, hoursView, minutesView; 
+	EditText numberOfContactsView, hoursView, minutesView;
 	String numberOfContacts, hours, minutes;
 	Button submitButton, abortButton;
 
 	/**
-	 * set the variables to the text fields and the button
-	 * set event listener
+	 * set the variables to the text fields and the button set event listener
 	 */
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.question_layout);
-        
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.question_layout);
+
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this.getBaseContext());
 		final String usercode = preferences.getString("usercode", "");
-        
-        numberOfContactsView = (EditText) findViewById(R.id.editText1);
-        hoursView = (EditText) findViewById(R.id.EditText01);
-        minutesView = (EditText) findViewById(R.id.EditText02);
-        
-        minutesView.setOnKeyListener(this);
-        hoursView.setOnKeyListener(this);
-        numberOfContactsView.setOnKeyListener(this);
-        
-        submitButton = (Button) findViewById(R.id.button1);
-        abortButton = (Button) findViewById(R.id.Button01);
-        
-        /**
+
+		numberOfContactsView = (EditText) findViewById(R.id.editText1);
+		hoursView = (EditText) findViewById(R.id.EditText01);
+		minutesView = (EditText) findViewById(R.id.EditText02);
+
+		submitButton = (Button) findViewById(R.id.button1);
+		abortButton = (Button) findViewById(R.id.Button01);
+
+		/**
          * 
          */
-        submitButton.setOnClickListener(new OnClickListener() {
-			
+		submitButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				if(hours == "" || minutes == "" || numberOfContacts==""){
+
+				numberOfContacts = numberOfContactsView.getText().toString();
+				hours = hoursView.getText().toString();
+				minutes = minutesView.getText().toString();
+
+				if (hours.equals("") || minutes.equals("")
+						|| numberOfContacts.equals("")) {
 					showErrorMessager("Fehlende Eingabe");
 				} else {
-					String[] line = new String[8];//data which will be written into a csv file
-					line[0]=usercode;//user code
+					String[] line = new String[8];// data which will be written
+													// into a csv file
+					line[0] = usercode;// user code
 					Date date = new Date();
-					line[1]= new SimpleDateFormat("dd.MM.yyyy").format(date);//date
-					line[2]="";//alert time
-					line[3]= new SimpleDateFormat("hh:mm").format(date);//answer time
-					line[4]="0";//wasn't aborted
-					line[5]=numberOfContacts;
-					line[6]=hours;
-					line[7]=minutes;
+					line[1] = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(date);// date
+					line[2] = "";// alert time
+					line[3] = new SimpleDateFormat("hh:mm", Locale.getDefault()).format(date);// answer
+																			// time
+					line[4] = "0";// wasn't aborted
+					line[5] = numberOfContacts;
+					line[6] = hours;
+					line[7] = minutes;
 					String filename = usercode + ".csv";
 					try {
 						CSVWriter.writeLine(line, filename);
@@ -76,28 +74,29 @@ public class QuestionsActivity extends Activity implements OnKeyListener {
 					}
 					finishActivity();
 				}
-				
+
 			}
 		});
-        
-        /**
-         * action when the user aborts the question 
-         */
-        abortButton.setOnClickListener(new OnClickListener() {
-			
+
+		/**
+		 * action when the user aborts the question
+		 */
+		abortButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				String[] line = new String[8];//data which will be written into a csv file
-				line[0]="";//user code
+				String[] line = new String[8];// data which will be written into
+												// a csv file
+				line[0] = usercode;// user code
 				Date date = new Date();
-				line[1]= new SimpleDateFormat("dd.MM.yyyy").format(date);//date
-				line[2]="";//alert time
-				line[3]= "-1";//answer time
-				line[4]="1";//was aborted
-				line[5]="-1";
-				line[6]="-1";
-				line[7]="-1";
-				String filename = ".csv";
+				line[1] = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(date);// date
+				line[2] = "";//TODO: alert time
+				line[3] = "-77";// answer time
+				line[4] = "1";// was aborted
+				line[5] = "-77";
+				line[6] = "-77";
+				line[7] = "-77";
+				String filename = usercode + ".csv";
 				try {
 					CSVWriter.writeLine(line, filename);
 				} catch (FileNotFoundException e) {
@@ -107,50 +106,22 @@ public class QuestionsActivity extends Activity implements OnKeyListener {
 			}
 		});
 
-    }
-	
+	}
+
 	/**
 	 * shows alert dialogue, for debugging purposes
+	 * 
 	 * @param message
 	 */
-	private void showErrorMessager(String message){
-		Context context = getApplicationContext();
-		int time = 3000;
-		
-		Toast toast = Toast.makeText(context, message, time);
-		toast.show();
+	private void showErrorMessager(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG)
+				.show();
 	}
-	
+
 	/**
 	 * closes the activity
 	 */
-	private void finishActivity(){
+	private void finishActivity() {
 		this.finish();
 	}
-
-	@Override
-	/**
-	 * onKey event listener for the text fields
-	 * get the string and store it in the variables
-	 */
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		
-		if(v==numberOfContactsView){
-			String text = numberOfContactsView.getText().toString();
-			numberOfContacts=text;
-		}
-		
-		if(v==hoursView){
-			String text = hoursView.getText().toString();
-			hours=text;
-		}
-
-		if(v==minutesView){
-			String text = minutesView.getText().toString();
-			minutes=text;
-		}
-		
-		return false;
-	}	
-	
 }
