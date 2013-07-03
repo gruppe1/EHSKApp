1ae9313880177ef22b2a581d35c2121a446941ff
Darius Brückers <darius@brueckers>
2013-07-03 20:22:02 +0200

    1. Merge :)
    Jippi, Funktioniert

--- app/src/ovgu/gruppe1/ehskapp/MainActivity.java
+++ app/src/ovgu/gruppe1/ehskapp/MainActivity.java
@@ -1,123 +1,12 @@
-<<<<<<< HEAD
 package ovgu.gruppe1.ehskapp;
-
-import java.io.FileNotFoundException;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.os.Bundle;
-import android.os.Environment;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends Activity {
-	
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
-		String usercode = preferences.getString("usercode", "");
-		Editor edit = preferences.edit();
-		Log.d("Main", ""+ usercode.length());
-				
-		if (usercode.length() != 0) {
-			
-			String data = usercode + ".csv";
-			Log.d("Main", ""+ CSVWriter.existsFile(data));
-			if (CSVWriter.existsFile(data) == false) {
-//				edit.clear();
-				edit.commit();
-			}
-			
-		}
-		
-		preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
-		usercode = preferences.getString("usercode", "");
-		
-		if (usercode.length() == 0) {
-			
-			Intent popupIntent = new Intent(this, UserCodeActivity.class);
-			startActivity(popupIntent);
-		}
-				
-
-		Button btn_usercode = (Button) findViewById(R.id.btn_usercode);
-		btn_usercode.setOnClickListener(new OnClickListener() {
-
-
-			@Override
-			public void onClick(View v) {
-				Intent popupIntent = new Intent(MainActivity.this, UserCodeActivity.class);
-				startActivity(popupIntent);
-			}
-		});
-		
-		
-		
-		
-		Button btn_preferences = (Button) findViewById(R.id.btn_preferences);
-		btn_preferences.setOnClickListener(new OnClickListener() {
-		
-			
-			@Override
-			public void onClick(View v) {
-
-
-			}
-		});
-	
-		Button btn_popup = (Button) findViewById(R.id.btn_time_chooser);
-		btn_popup.setOnClickListener(new OnClickListener() {
-
-
-			@Override
-			public void onClick(View v) {
-				Intent popupIntent = new Intent(MainActivity.this, TimeChooserActivity.class);
-				startActivity(popupIntent);
-			}
-		});
-		
-		Button btn_questions = (Button) findViewById(R.id.btn_questions);
-		btn_questions.setOnClickListener(new OnClickListener() {
-
-
-			@Override
-			public void onClick(View v) {
-				Intent popupIntent = new Intent(MainActivity.this, QuestionsActivity.class);
-				startActivity(popupIntent);
-			}
-		});
-
-		
-	}
-	
-
-=======
-package ovgu.gruppe1.ehskapp;
-
-import java.io.FileNotFoundException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -167,15 +56,26 @@ public class MainActivity extends Activity {
 //				startActivity(intent);
 			}
 		});
-	
-		Button btn_popup = (Button) findViewById(R.id.btn_popup);
-		btn_popup.setOnClickListener(new OnClickListener() {
+
+		Button btn_time_chooser = (Button) findViewById(R.id.btn_time_chooser);
+		btn_time_chooser.setOnClickListener(new OnClickListener() {
 
 
 			@Override
 			public void onClick(View v) {
-				Intent popupIntent = new Intent(MainActivity.this, Alarm.class);
-				startActivity(popupIntent);
+				Intent time_chooserIntent = new Intent(MainActivity.this, TimeChooserActivity.class);
+				startActivity(time_chooserIntent);
+			}
+		});
+		
+		Button btn_alarm = (Button) findViewById(R.id.btn_alarm);
+		btn_alarm.setOnClickListener(new OnClickListener() {
+
+
+			@Override
+			public void onClick(View v) {
+				Intent alarmIntent = new Intent(MainActivity.this, Alarm.class);
+				startActivity(alarmIntent);
 			}
 		});
 		
@@ -216,5 +116,4 @@ public class MainActivity extends Activity {
 	    return super.onOptionsItemSelected(item);
 	} */
 
->>>>>>> origin/working_activities
 }
